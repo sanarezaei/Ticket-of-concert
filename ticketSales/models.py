305f929c696b680aec0833408d5fdf5c2e9628d3
= <sanare@gmail.com>
2024-06-13 22:57:39 +0330

    concert and location models

--- ticketSales/models.py
+++ ticketSales/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ConsertModel(models.Model):
+    name = models.CharField(max_length=100)
+    singer_name = models.CharField(max_length=100)
+    length = models.IntegerField()
+    
+    def __str__(self):
+        return self.singer_name
+    
+class locationModel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=11)
+    capacity = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
